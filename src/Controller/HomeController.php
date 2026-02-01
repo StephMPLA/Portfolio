f3454f83eed80d707b334b07d5b089e2303e1e6a
StephMPLA <stephmpla@gmail.com>
2026-02-01 17:48:15 +0100

    Implement secure contact form (rate limiting + honeypot), initial portfolio UI version (WIP)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,15 +4,49 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\RateLimiter\RateLimiterFactory;
 use Symfony\Component\Routing\Attribute\Route;
-
+use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
 final class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
-    {
+    #[Route('/', name: 'app_home')]
+    public function index(
+        Request $request,
+        MailerInterface $mailer,
+        RateLimiterFactory $contactFormLimiter
+    ): Response    {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // honeypot
+            if ($form->get('website')->getData()) {
+                return new Response('Spam détecté', 400);
+            }
+
+            // rate limit
+            $limit = $contactFormLimiter->create($request->getClientIp());
+            if (!$limit->consume()->isAccepted()) {
+                return new Response('Too many attempts', 429);
+            }
+
+            $data = $form->getData();
+
+            $email = (new Email())
+                ->from($data['email'])
+                ->to('ton@email.fr')
+                ->subject('Message portfolio')
+                ->text($data['message']);
+
+            $mailer->send($email);
+        }
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'form' => $form->createView(),
         ]);
     }
+
 }
